4b62bad0b8cdc840648ff649bbf518e9e1b9209a
Michael Comella <michael.l.comella@gmail.com>
2015-04-28 16:48:18 -0700

    New android project.

---
+++ src/me/mcomella/applicationid/ExtraApplicationId.java
@@ -0,0 +1,15 @@
+package me.mcomella.applicationid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExtraApplicationId extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
