ff061910873b07fcd45c14d557e70f8ea96b027a
Michael Comella <michael.l.comella@gmail.com>
2015-04-29 15:01:09 -0700

    Add 2 more links to represent non-application ID links.

--- src/me/mcomella/applicationid/ExtraApplicationId.java
+++ src/me/mcomella/applicationid/ExtraApplicationId.java
@@ -14,6 +14,8 @@ public class ExtraApplicationId extends Activity
 
     private final String LINK_1_URL = "http://kotaku.com";
     private final String LINK_2_URL = "https://en.wikipedia.org/wiki/IU_%28singer%29";
+    private final String LINK_3_URL = "http://indefinitelywild.gizmodo.com/";
+    private final String LINK_4_URL = "https://mozilla.org";
 
     /** Called when the activity is first created. */
     @Override
@@ -22,29 +24,42 @@ public class ExtraApplicationId extends Activity
         setContentView(R.layout.main);
 
         final View link1 = findViewById(R.id.link1);
-        link1.setOnClickListener(new ApplicationIDOnClickListener(LINK_1_URL));
+        link1.setOnClickListener(new IntentOnClickListener(LINK_1_URL, true));
 
         final View link2 = findViewById(R.id.link2);
-        link2.setOnClickListener(new ApplicationIDOnClickListener(LINK_2_URL));
+        link2.setOnClickListener(new IntentOnClickListener(LINK_2_URL, true));
+
+        final View link3 = findViewById(R.id.link3);
+        link3.setOnClickListener(new IntentOnClickListener(LINK_3_URL, false));
+
+        final View link4 = findViewById(R.id.link4);
+        link4.setOnClickListener(new IntentOnClickListener(LINK_4_URL, false));
     }
 
-    private Intent createIntentWithApplicationID(String url) {
+
+    private Intent createIntent(String url, boolean withApplicationId) {
         final Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(url));
-        intent.putExtra(Browser.EXTRA_APPLICATION_ID, getPackageName());
+
+        if (withApplicationId) {
+            intent.putExtra(Browser.EXTRA_APPLICATION_ID, getPackageName());
+        }
+
         return intent;
     }
 
-    private class ApplicationIDOnClickListener implements OnClickListener {
+    private class IntentOnClickListener implements OnClickListener {
         private final String url;
+        private final boolean withApplicationId;
 
-        public ApplicationIDOnClickListener(String url) {
+        public IntentOnClickListener(String url, boolean withApplicationId) {
             this.url = url;
+            this.withApplicationId = withApplicationId;
         }
 
         @Override
         public void onClick(View v) {
-            final Intent intent = createIntentWithApplicationID(url);
+            final Intent intent = createIntent(url, withApplicationId);
             startActivity(intent);
         }
     }
